d141375bc61d66f55cf79f61661f4d0ff270a601
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-18 19:01:50 +0000

    [Fix]seed値メンテ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Genre.create!(
-  name: "ケーキ"
-)
-Genre.create!(
-  name: "焼き菓子"
-)
-Genre.create!(
-  name: "プリン"
-)
-Genre.create!(
-  name: "キャンディ"
-)
+Genre.create!([
+  { name: "ケーキ" },
+  { name: "焼き菓子" },
+  { name: "プリン" },
+  { name: "キャンディ" }
+])
 
 Item.create!(
   name: "いちごのショートケーキ（ホール）",
@@ -26,25 +20,86 @@ Item.create!(
   is_active: true,
   genre_id: 1,
 )
-10.times do |n|
-  Item.create!(
-  name: "Test#{n + 1}",
-  introduction: "Test#{n + 1}を贅沢に使用しています。",
-  price: "1000",
+Item.create!(
+  name: "ストロベリーショートケーキ",
+  introduction: "甘酸っぱいストロベリーがたっぷりのせられた、ふんわりとしたショートケーキです。",
+  price: 1800,
+  is_active: true,
+  genre_id: 1,
+)
+Item.create!(
+  name: "チョコマーブルパウンドケーキ",
+  introduction: "リッチなチョコレートとバニラの風味が絶妙に絡み合った、しっとりとしたパウンドケーキです。",
+  price: 1500,
+  is_active: true,
+  genre_id: 1,
+)
+Item.create!(
+  name: "シナモンロール",
+  introduction: "香り高いシナモンがふんだんに使われた、ほんのり甘くてもっちりとしたロールケーキです。",
+  price: 300,
   is_active: true,
   genre_id: 2,
-  )
-end
+)
+Item.create!(
+  name: "カラメルプリン",
+  introduction: "なめらかなカラメルソースが広がる、濃厚でまろやかなプリンです。",
+  price: 400,
+  is_active: true,
+  genre_id: 3,
+)
+Item.create!(
+  name: "ブルーベリーハードキャンディ",
+  introduction: "爽やかなブルーベリーの風味が詰まった、キャンディのクラシックなお楽しみです。",
+  price: 100,
+  is_active: true,
+  genre_id: 4,
+)
+Item.create!(
+  name: "クランチィチョコチップクッキー",
+  introduction: "サクサクとした食感の中に、たっぷりのチョコチップが入った、香ばしいクッキーです。",
+  price: 200,
+  is_active: true,
+  genre_id: 2,
+)
+Item.create!(
+  name: "マンゴープリン",
+  introduction: "豊かなマンゴーの風味が広がる、滑らかでフルーティーなプリンです。",
+  price: 500,
+  is_active: true,
+  genre_id: 3,
+)
+Item.create!(
+  name: "ベリーパーティーケーキ",
+  introduction: "様々なベリーが彩りを添えた、楽しいパーティーにぴったりのケーキです。",
+  price: 2000,
+  is_active: true,
+  genre_id: 1,
+)
+Item.create!(
+  name: "ヘーゼルナッツプリン",
+  introduction: "ほろ苦いチョコレートと香ばしいヘーゼルナッツが絶妙なバランスのプリンです。",
+  price: 450,
+  is_active: true,
+  genre_id: 3,
+)
+Item.create!(
+  name: "レモンサワーキャンディ",
+  introduction: "さわやかなレモンの酸味と甘さが楽しめる、爽快なサワーキャンディです。",
+  price: 80,
+  is_active: true,
+  genre_id: 4,
+)
 
-# Customer.create!(
-#   last_name: "姓",
-#   first_name: "名",
-#   last_name_kana: "セイ",
-#   first_name_kana: "メイ",
-#   postcode: "1234567",
-#   address: "123456",
-#   phone_number: "123456",
-#   email: "aaa@aaa",
-#   encrypted_password: "aaaaaa",
-#   is_deleted: false
-# )
+Customer.create!(
+  last_name: "山田",
+  first_name: "太郎",
+  last_name_kana: "ヤマダ",
+  first_name_kana: "タロウ",
+  postcode: "123-4567",
+  address: "東京都渋谷区1-2-3",
+  phone_number: "090-1234-5678",
+  email: "test@example.com",
+  password: "password",
+  is_deleted: false
+)
