5a7b442e7a3a5bd15c41ff06634b74db016f5452
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-18 03:56:45 +0000

    コンフリクト

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  device_for :admin
-  
-   namespace :admin do
-    resources :items
-    resources :genres,only {index,create,edit,update}
-  end
-  
-  
 end
