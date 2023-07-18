f9c0b527aba166568d24ad39aa2e2c39b5d6538e
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-07-18 23:24:40 +0000

    AddPermitEmail

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,6 +20,6 @@ class Public::CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(
-      :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
+      :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
   end
 end
