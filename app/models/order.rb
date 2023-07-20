f1c26091d9e420193cfb410ee78d6f33f4d55b15
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-20 14:05:02 +0000

    [Fix]支払方法の記載日本語化

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,6 @@ class Order < ApplicationRecord
 
   has_many :items, through: :order_details
 
-  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
 
 end
