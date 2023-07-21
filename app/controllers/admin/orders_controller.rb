64ef3d001df30dd1e9fcb401b6ba4a86e3901927
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-21 08:55:23 +0000

    注文ステータスb

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,10 +6,17 @@ class Admin::OrdersController < ApplicationController
   end
   
   def update
+     @order = Order.find(params[:id])
+     @order.update(order_params)
+     redirect_to admin_order_path(@order)
   end
 end
 
  private
    def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+   end
+   
+   def order_params
+       params.require(:order).permit(:postcode, :address, :receiver, :postage, :status)
    end
