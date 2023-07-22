6e593fcfc224ef308259f9b3a40ec511a27fb059
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-07-22 07:13:52 +0000

    退会機能の修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application"; 
+
 
 
 import Rails from "@rails/ujs"
@@ -14,6 +11,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
