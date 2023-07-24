9a181b9680f7c223ecdfc39c0e87740596ac4c70
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-24 17:23:39 +0000

    [Add]画像gem対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,18 +12,18 @@ class Item < ApplicationRecord
   def with_tax_price#税込み価格
    (price * (1 + TAX_RATE * 0.01)).floor
   end
-  
+
   def total_item_amount #アイテム合計金額
     order_details.sum { |order_detail| order_detail.subtotal }
   end
 
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.png')
       image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
-    image
+     image.variant(resize_to_limit: [width, height]).processed
   end
 
   validates :is_active, inclusion: {in: [true, false]} #販売ステータス
