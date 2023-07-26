ef9dbbb036255795f0bab77a9433afca7ab4fe74
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-26 15:45:52 +0000

    [Fix]Admin注文履歴詳細の購入者表記&Public配送先フォームサイズ変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,11 @@
 class Admin::OrdersController < ApplicationController
 
   def show
-     @customer = Customer.find(params[:id])
      @order = Order.find(params[:id])
-     @item = Item.find(params[:id])
+     @customer = @order.customer
      @order_items = @order.order_details.all #注文ごとに紐づいた商品の表示
      @order_details = @order.order_details.all
      @total_item_amount = @order_details.sum { |order_detail| order_detail.subtotal }
-     @order = Order.find(params[:id])
   end
 
   def update #注文ステータスの更新
